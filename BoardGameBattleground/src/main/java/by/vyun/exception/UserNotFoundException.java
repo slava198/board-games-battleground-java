df049a0e6be97514e370316ecbda79955cf33663
vyun_slava <slava198@tut.by>
2020-03-12 01:29:14 +0300

    add: UserRestController, exception package; all known bugs fixed

---
+++ BoardGameBattleground/src/main/java/by/vyun/exception/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package by.vyun.model;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="No such User")  // 404
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String login) {
+        super(login);
+    }
+    // ...
+}
